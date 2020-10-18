0561e88846fd7a6445e480962f769389b06c7051
AbbyWood22 <69914691+AbbyWood22@users.noreply.github.com>
2020-10-18 14:29:04 -0600

    homework_9

---
+++ homework_9/sketch.js
@@ -0,0 +1,24 @@
+function setup() {
+  createCanvas(300,500);
+}
+
+function draw() {
+  background(123,23,129);
+  text('Abbys simple portrait', 10, 30);
+  circle(150,200,200);
+  triangle(100, 450, 150, 300, 206, 450);
+  square(180,150,30);
+  square(100,150,30);
+  point(150, 250);
+  point(151, 250);
+  point(152, 250);
+  point(153, 250);
+  point(154, 250);
+  point(155, 250);
+  point(156, 250);
+  point(157, 250);
+  point(158, 250);
+  line(150, 300, 206, 350);
+  line(150, 300, 106, 350);
+  text('By: Abby Wood', 10, 490);
+}
