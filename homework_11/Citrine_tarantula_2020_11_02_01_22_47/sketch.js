6014b7630e9e8c51e551e102045f209a6b8cfdcf
AbbyWood22 <69914691+AbbyWood22@users.noreply.github.com>
2020-11-02 12:20:11 -0700

    homework_11

---
+++ homework_11/Citrine_tarantula_2020_11_02_01_22_47/sketch.js
@@ -0,0 +1,63 @@
+var x = 100;
+var y = 100;
+var a = 130;
+var b = 140;
+var c = 450;
+var d = 150;
+
+function setup()
+{
+    createCanvas(800,600);
+}
+function draw() 
+{
+  background(123,28,38);
+  circle(x,y,50);
+  square(a,b,d);
+  square(c,b,d);
+}
+function keyPressed() 
+{
+  if (key == 'd') 
+  {
+    x+=15;
+  } 
+  else if (key == 'a') 
+  {
+    x-=15;
+  }
+  else if (key == 'w') 
+  {
+    y+=15;
+  }
+  else if (key == 's') 
+  {
+    y-=15;
+  }
+}
+function isHit()
+{
+  if(x == a || x == c)
+    text('OUCH!', 10, 30);
+  else if(y == b)
+    text('RATS!', 10, 30);
+}
+function switchSides()
+{
+  if(x == 800)
+    x = 10
+  else if( y == 600)
+    y = 10   
+}
+function mouseClicked() 
+{  
+  mousex = mouseX;
+  mousey = mouseY;
+  if (mouseX == True)
+    square(500,500,75);
+}
+function isExit()
+{
+  if (x == 450 && y == 350)
+    text('YOU WIN!', 10, 60);
+}
